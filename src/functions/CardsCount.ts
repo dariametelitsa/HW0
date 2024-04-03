8bf680a0108c30b958ad8657068a537ddfdcafc9
dariametelitsa <dariametelitsa@gmail.com>
2024-04-03 17:59:14 +0300

    codewars kata was solved (findOdd)

--- src/functions/CardsCount.ts
+++ src/functions/CardsCount.ts
@@ -1,13 +1,13 @@
 export const cardsCount = (cards: Array<string>) => {
-    return cards.reduce((sum,el) => {
-        if(/^[b-zB-Z]+$/.test(el)) {
+    return cards.reduce((sum, el) => {
+        if (/^[b-zB-Z]+$/.test(el)) {
             return sum + 10;
         }
-        if(/^[aA]+$/.test(el)){
+        if (/^[aA]+$/.test(el)) {
             return (sum + 11) <= 21 ? (sum + 11) : (sum + 1);
         }
         return sum + +el;
-    },0);
+    }, 0);
 }
 
 function disemvowel(str: string) {
@@ -15,6 +15,17 @@ function disemvowel(str: string) {
 }
 
 function findOdd(arr: Array<number>) {
-
-    return 0;
+    const obj:any = {}
+    arr.forEach((el, index) => {
+        if(!obj.hasOwnProperty(el)) {
+            obj[el] = 1;
+        }
+        else obj[el]++;
+    });
+    for (let key in obj) {
+        if(obj[key] % 2 === 1) {
+            return +key;
+        }
+    }
 }
+
