e743564a10ce6ae06138ef79e6fb4b483df3f4fb
dariametelitsa <dariametelitsa@gmail.com>
2024-04-08 11:26:24 +0300

    changed generate cards mechanic

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,52 +4,55 @@ import { Button } from "./Button";
 import myimage from './desert.jpg';
 
 
-export function Card() {
+export type CardTypes = {
+    title: string;
+    text: string;
+}
+export function Card(props: CardTypes) {
   return (
     <CardStyle>
       <Image width='280' height='170' src={myimage} alt="The desert." />
       <div>
-        <Headline>Headline</Headline>
-        <Text>Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in venen.</Text>  
+        <Headline>{props.title}</Headline>
+        <Text>{props.text}</Text>
         <Button as='a' href="#">See more</Button>
         <Button outline>Save</Button>
-        {/* <Button href="#">See more</Button>
-        <Button as='button' outline>Save</Button> */}
       </div>
     </CardStyle>
   );
 }
 
 const CardStyle = styled.div`
-  /* max-width: min-content; */
-  min-width: 20%;
-  padding: 10px;
-  border-radius: 15px;
-  box-shadow: 0 4px 20px 5px rgba(0, 0, 0, 0.1);
+    /* max-width: min-content; */
+    align-self: start;
+    min-width: 20%;
+    padding: 10px;
+    border-radius: 15px;
+    box-shadow: 0 4px 20px 5px rgba(0, 0, 0, 0.1);
 
-  div {
-    padding: 20px 10px 0;
+    div {
+        padding: 20px 10px 0;
 
-    @media screen and (max-width: 1000px){
-      display: flex;
-      flex-direction: column;
-      padding: 10px 0 0 0;
+        @media screen and (max-width: 1000px) {
+            display: flex;
+            flex-direction: column;
+            padding: 10px 0 0 0;
 
-      a {
-        margin-right: 0;
-      }
+            a {
+                margin-right: 0;
+            }
+        }
     }
-  }
 
-  Button {
-    margin-bottom: 12px;
-  }
+    Button {
+        margin-bottom: 12px;
+    }
 
-  a {
-    text-align: center;
-    margin-right: 12px;
-    margin-bottom: 12px;
-  }
+    a {
+        text-align: center;
+        margin-right: 12px;
+        margin-bottom: 12px;
+    }
 `
 
 const Headline = styled.h2`
--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,43 +1,70 @@
 import styled from "styled-components";
-import { Card } from "./Card";
+import { Card, CardTypes } from "./Card";
 import { cardTheme } from "../Styles/Theme.styles";
+import { useState } from "react";
+
+
+const cardsData: CardTypes[] = [
+    {
+        title: 'Header',
+        text: 'Весьма перспективной представляется гипотеза, высказанная И.Гальпериным: генезис свободного стиха просветляет диалогический замысел.',
+    },
+    {
+        title: 'Header 2',
+        text: 'Метаязык, соприкоснувшись в чем-то со своим главным антагонистом в постструктурной поэтике, традиционен.',
+    },
+    {
+        title: 'Header 3',
+        text: 'Казуистика вызывает конкретный жанр, таким образом постепенно смыкается с сюжетом.',
+    },
+    {
+        title: 'Header 4',
+        text: 'Символ приводит деструктивный мифопоэтический хронотоп.',
+    },
+    {
+        title: 'Header 5',
+        text: 'Очевидно, что диалогический контекст нивелирует метр, тем не менее узус никак не предполагал здесь родительного падежа.',
+    },
+]
 
 export function GalleryContainer() {
-  return <div>
-   <Headline>Gallery</Headline>
-    <Gallery columns={4}>
-      {cardGenerate(7)}
-    </Gallery>
-  </div>
+    let [next, setNext] = useState('0');
+
+    return <div>
+        <Headline>Gallery</Headline>
+        <Gallery columns={4}>
+            {cardGenerate(cardsData.length)}
+        </Gallery>
+    </div>
 }
 
 
 type GalleryPropsType = {
-  columns?: 2 | 3 | 4 | 5,
+    columns?: 2 | 3 | 4 | 5,
 }
 const Gallery = styled.div<GalleryPropsType>`
-  margin: auto;
-  max-width: 1000px;
-  width: 90%;
-  display: grid;
-  gap: 20px;
-  grid-template-columns: repeat(${props => props.columns || 3}, 1fr);
+    margin: auto;
+    max-width: 1000px;
+    width: 90%;
+    display: grid;
+    gap: 20px;
+    grid-template-columns: repeat(${props => props.columns || 3}, 1fr);
 
 `
 
 function cardGenerate(count: number = 6) {
-  const cards = [];
-  for (let i = 0; i < count; i++) {
-    cards.push(<Card />);
-  }
-  return cards;
+    const cards = [];
+    for (let i = 0; i < count; i++) {
+        cards.push(<Card title={cardsData[i].title} text={cardsData[i].text}/>);
+    }
+    return cards;
 }
 
 const Headline = styled.h1`
-font-size: 35px;
-  margin-top: 70px;
-  margin-bottom: 30px;
-  color: ${cardTheme.colors.grey.dark};
-  text-align: center;
-  font-weight: 700;
+    font-size: 35px;
+    margin-top: 70px;
+    margin-bottom: 30px;
+    color: ${cardTheme.colors.grey.dark};
+    text-align: center;
+    font-weight: 700;
 `
