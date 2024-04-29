a1e5a78a261835972d5e6e107f051b4599c42900
dariametelitsa <dariametelitsa@gmail.com>
2024-04-29 15:28:26 +0300

    tested different input interaction v2

--- src/components/input.stories.tsx
+++ src/components/input.stories.tsx
@@ -1,5 +1,5 @@
 import { action } from "@storybook/addon-actions";
-import { useRef, useState } from "react";
+import { ChangeEvent, useRef, useState } from "react";
 
 export default {
     title: 'input',
@@ -37,6 +37,34 @@ export const GetValueUncontrolledInputByButtonPress = () => {
     )
 }
 
-    
 export const ControlledInputWithFixedValue =() =>  <input value={'Pryanichek'} />
+
+export const ControlledInput = () => {
+    const [parentValue, setParentValue] = useState("");
+    const onChange=(event: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(event.currentTarget.value)
+    }
+    return <input value={parentValue} onChange={onChange} />
+}
+
+export const ControlledCheckbox = () => {
+    const [parentValue, setParentValue] = useState(true);
+    const onChange=(event: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(event.target.checked);
+    }
+    return <input type={"checkbox"} onChange={onChange} checked={parentValue} />
+}
+
+export const ControlledSelect = () => {
+    const [parentValue, setParentValue] = useState<string | undefined>('2');
+    const onChange=(event: ChangeEvent<HTMLSelectElement>) => {
+        setParentValue(event.target.value);
+    }
+    return <select value={parentValue} onChange={onChange}>
+        <option>none</option>
+        <option value={'1'}>Minsk</option>
+        <option value={'2'}>Varshava</option>
+        <option value={'3'}>Karaganda</option>
+    </select>
+}
 
