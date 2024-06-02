4f9da21038777c5dcb8a379c717dbd93aaa0c6e7
Daria Metelitsa <dariametelitsa@gmail.com>
2024-06-02 19:47:26 +0300

    useEffect clock

---
+++ src/components/clock/Clock.stories.tsx
@@ -0,0 +1,12 @@
+import { Clock } from "./Clock";
+import { useEffect, useState } from "react";
+import * as React from "react";
+
+export default {
+    title: 'Clock',
+    component: Clock
+}
+
+export const ClockExample = () => {
+    return <Clock/>
+};
---
+++ src/components/clock/Clock.tsx
@@ -0,0 +1,30 @@
+// @flow
+import * as React from 'react';
+import { useEffect, useState } from 'react';
+
+function get2digitsString(date: number) {
+    return date < 10
+        ? '0' + date
+        : date;
+}
+
+type ClockProps = {};
+export const Clock: React.FC = (props: ClockProps) => {
+    const [date, setDate] = useState(new Date);
+
+    useEffect(() => {
+        setInterval(() => {
+            setDate(new Date);
+        }, 1000);
+    }, []);
+
+    return (
+        <div>
+            <span>{get2digitsString(date.getHours())}</span>
+            :
+            <span>{get2digitsString(date.getMinutes())}</span>
+            :
+            <span>{get2digitsString(date.getSeconds())}</span>
+        </div>
+    );
+};
