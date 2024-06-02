d732acf9d32b5f9aec0767c98d933e004ecf3a8a
Daria Metelitsa <dariametelitsa@gmail.com>
2024-06-02 19:27:57 +0300

    useEffect experiments

--- src/functions/useEffect.stories.tsx
+++ src/functions/useEffect.stories.tsx
@@ -32,3 +32,53 @@ export const UseEffectExample = () => {
         Hello, {counter}
     </>
 };
+
+export const UseEffectSetTimeoutExample = () => {
+    const [counter, setCounter] = useState(1);
+    const [fake, setFake] = useState(1);
+
+    useEffect(() => {
+        setTimeout(() => {
+            console.log('useEffect first render and every counter change');
+            document.title = counter.toString();
+        }, 2000)
+    }, [counter]);
+
+    return <>
+        <h2>UseEffect</h2>
+        <button onClick={() => setCounter(prev => prev + 1)}>+</button>
+        Hello, {counter}
+    </>
+};
+
+export const UseEffectSetIntervalExample = () => {
+    const [counter, setCounter] = useState(1);
+
+    useEffect(() => {
+        setInterval(() => {
+            setCounter(prev => prev + 1);
+            console.log('setTimeout');
+            document.title = counter.toString();
+        }, 1000)
+    },[]);
+
+    return <>
+        <h2>UseEffect</h2>
+        Hello, counter: {counter}
+    </>
+};
+
+export const UseEffectClock = () => {
+    const [date, setDate] = useState(new Date);
+
+    useEffect(() => {
+        setInterval(() => {
+            setDate(prev => new Date);
+            console.log('setTimeout');
+        }, 1000)
+    },[]);
+
+    return <>
+        <time>Time: {date.getHours()} : {date.getMinutes()} : {date.getSeconds()}</time>
+    </>
+};
